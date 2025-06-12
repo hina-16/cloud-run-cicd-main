3788b4436f444c22458da4214682579ab30e7786
Hina Kausar <hinakausar.khan@fracta.ai>
2025-06-12 07:02:24 +0000

    Second Commit

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ version = os.environ.get("VERSION", "dev")
 
 @app.route("/")
 def hello():
-    return f"Hello from Cloud Run Welcomes you! Version: {version}"
+    return f"Hello from Cloud Run Welcomes you! This is the new version after editing  Version: {version}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
